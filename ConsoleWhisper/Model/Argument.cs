33ee156ee6fda6fe1853a5820ce7c4cc95826e02
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2023-06-21 18:30:14 +0800

    Fix typo. Remove GPU argument.

--- ConsoleWhisper/Model/Argument.cs
+++ ConsoleWhisper/Model/Argument.cs
@@ -8,7 +8,7 @@ namespace ConsoleWhisper.Model {
 		[Value(3)]
 		public IEnumerable<string> Files { get; set; }
 
-		[Option('m', "model", Required = false, Hidden = false, Default = "small", HelpText = "Whiper model: base, tiny, small, medium, large.")]
+		[Option('m', "model", Required = false, Hidden = false, Default = "small", HelpText = "Whisper model: base, tiny, small, medium, large.")]
 		[Value(0, Min = 0, Max = 1, Required = false)]
 		public string ModelType { get; set; }
 
@@ -16,7 +16,7 @@ namespace ConsoleWhisper.Model {
 		[Value(1, Min = 0, Max = 1, Required = false)]
 		public string OutputDir { get; set; }
 
-		[Option('g', "gpu", Required = false, Hidden = false, HelpText = "Use GPU, using CPU if not specified.")]
+		[Option('g', "gpu", Required = false, Hidden = true, HelpText = "Currently not implemented.")]
 		[Value(2, Required = false, Default = false)]
 		public bool GPU { get; set; }
 
@@ -29,7 +29,7 @@ namespace ConsoleWhisper.Model {
 				throw new ArgumentException(message: $"Whisper model type \"{ModelType}\" is not supported.");
 		}
 
-		internal const int SupportedArgumentsCount = 6;
+		internal const int SupportedArgumentsCount = 5;
 
 		private static readonly HashSet<string> SupportedModels = new() { "base", "tiny", "small", "medium", "large" };
 	}
